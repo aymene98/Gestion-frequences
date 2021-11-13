d8c36f90431dd47d4cb27bc4e3eede8b88573d97
aymene <aymene98@icloud.com>
2021-11-13 15:35:24 +0100

    adding file to run choco

--- run-choco.py
+++ run-choco.py
@@ -5,22 +5,25 @@ path = "./output/log-m1"
 files = [f for f in listdir(path)]
 print(len(files))
 for file in files[:1]:
-    name  = path + '/' + file
-    commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
-    os.system(commande)
+    if file.endswith('.xml'):
+        name  = path + '/' + file
+        commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        os.system(commande)
     
 path = "./output/log-m2"
 files = [f for f in listdir(path)]
 print(len(files))
 for file in files[:1]:
-    name  = path + '/' + file
-    commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
-    os.system(commande)
+    if file.endswith('.xml'):
+        name  = path + '/' + file
+        commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        os.system(commande)
     
 path = "./output/log-m3"
 files = [f for f in listdir(path)]
 print(len(files))
 for file in files[:1]:
-    name  = path + '/' + file
-    commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
-    os.system(commande)
+    if file.endswith('.xml'):
+        name  = path + '/' + file
+        commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        os.system(commande)
