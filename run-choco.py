a983ab762ece11f48397f58d91bb6197dbf83716
aymene <aymene98@icloud.com>
2021-11-13 18:08:40 +0100

    fixing bugs

--- run-choco.py
+++ run-choco.py
@@ -1,29 +1,35 @@
-import os, signal
+import os, time
 from os import listdir 
 
 path = "./output/log-m1"
 files = [f for f in listdir(path)]
 print(len(files))
-for file in files[:1]:
+for file in files:
     if file.endswith('.xml'):
         name  = path + '/' + file
-        commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        commande = "timeout 30s java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        start = time.time()
         os.system(commande)
+        print('\n',file, ' time:', time.time()-start)
     
 path = "./output/log-m2"
 files = [f for f in listdir(path)]
 print(len(files))
-for file in files[:1]:
+for file in files:
     if file.endswith('.xml'):
         name  = path + '/' + file
-        commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        commande = "timeout 30s java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        start = time.time()
         os.system(commande)
+        print('\n',file, ' time:', time.time()-start)
     
 path = "./output/log-m3"
 files = [f for f in listdir(path)]
 print(len(files))
-for file in files[:1]:
+for file in files:
     if file.endswith('.xml'):
         name  = path + '/' + file
-        commande = "timeout 5m java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        commande = "timeout 30s java -jar choco-parsers-4.10.7-jar-with-dependencies.jar " + name
+        start = time.time()
         os.system(commande)
+        print('\n',file, ' time:', time.time()-start)
